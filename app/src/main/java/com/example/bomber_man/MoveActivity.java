f404aa28d1118b82d25f1e30f481a9a9a89109cb
yanagi-takuto <d7536987d@gmail.com>
2024-11-15 16:07:13 +0900

    kannseisitaraiina

--- app/src/main/java/com/example/bomber_man/MoveActivity.java
+++ app/src/main/java/com/example/bomber_man/MoveActivity.java
@@ -2,7 +2,7 @@ package com.example.bomber_man;
 
 public class MoveActivity {
     public static void Right_move(int x, int y) {
-        if((x += 1) < 13) {
+        if((x + 1) < 13) {
             if (GameActivity.blocks[y][x + 1] == 0) {
                 GameActivity.blocks[y][x] = 0;
                 GameActivity.blocks[y][x + 1] = 3;
@@ -18,7 +18,7 @@ public class MoveActivity {
         }
     }
     public static void up_move(int x, int y) {
-        if(0 <= (y -= 1)){
+        if(0 <= (y - 1)){
             if(GameActivity.blocks[y-1][x] == 0){
                 GameActivity.blocks[y][x] = 0;
                 GameActivity.blocks[y-1][x] = 3;
@@ -26,7 +26,7 @@ public class MoveActivity {
         }
     }
     public static void down_move(int x, int y) {
-        if((x += 1) < 10){
+        if((x + 1) < 10){
             if(GameActivity.blocks[y-3][x] == 0){
                 GameActivity.blocks[y][x] = 0;
                 GameActivity.blocks[y+1][x] = 3;
