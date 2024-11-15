4d2a6e61b1b6866cf339f82d6c8f75e57606f440
yanagi-takuto <d7536987d@gmail.com>
2024-11-15 16:34:30 +0900

    kannseisitaraiina

--- app/src/main/java/com/example/bomber_man/MoveActivity.java
+++ app/src/main/java/com/example/bomber_man/MoveActivity.java
@@ -6,43 +6,39 @@ public class MoveActivity {
             if (GameActivity.blocks[y][x + 1] == 1) {
                 GameActivity.blocks[y][x] = 1;
                 GameActivity.blocks[y][x + 1] = 3;
-            }return true;
+                return true;
+            }
         }
-        else {
-            return false;
-        }
+        return false;
     }
     public static boolean Left_move(int x, int y) {
         if(0 <= (x-1)){
             if(GameActivity.blocks[y][x-1] == 1){
                 GameActivity.blocks[y][x] = 1;
                 GameActivity.blocks[y][x-1] = 3;
-            }return true;
+                return true;
+            }
         }
-        else {
-            return false;
-        }
+        return false;
     }
     public static boolean up_move(int x, int y) {
         if(0 <= (y - 1)){
-            if(GameActivity.blocks[y-1][x] == 1){
+            if(GameActivity.blocks[y - 1][x] == 1){
                 GameActivity.blocks[y][x] = 1;
-                GameActivity.blocks[y-1][x] = 3;
-            }return true;
+                GameActivity.blocks[y - 1][x] = 3;
+                return true;
+            }
         }
-        else {
-            return false;
-        }
+        return false;
     }
     public static boolean down_move(int x, int y) {
-        if((x + 1) < 10){
-            if(GameActivity.blocks[y+1][x] == 1) {
+        if((y + 1) < 10){
+            if(GameActivity.blocks[y + 1][x] == 1) {
                 GameActivity.blocks[y][x] = 1;
                 GameActivity.blocks[y + 1][x] = 3;
-            }return true;
+                return true;
+            }
         }
-        else {
-            return false;
-        }
+        return false;
     }
 }
